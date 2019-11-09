6086fbd071d8427f5c8cd71f2f72be9d33d7ec14
Marvin John Mendoza <jomarlive@gmail.com>
2019-11-09 10:08:56 +0800

    Update extension.ts
    now it can fill up information

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,28 @@ import { Context } from 'mocha';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(commands.registerCommand('RMMV.createNewRMMVPlugin', async() => {
-		mvPluginGenerator('testtitle', 'yoshino', 'This is a test plugin');
+		const titleOutput = await window.showInputBox({
+			value: '',
+			placeHolder: 'Title of your plugin',
+			validateInput: text => {
+				return text === '' ? 'Do not leave this blank' : null;
+			}
+		});
+		const authorOutput = await window.showInputBox({
+			value: '',
+			placeHolder: `your name? Don't worry we won't transfer you to someoone's body`,
+			validateInput: text => {
+				return text === '' ? 'Do not leave this blank' : null;
+			}
+		});
+		const infoOutput = await window.showInputBox({
+			value: '',
+			placeHolder: `what your plugin can do?`,
+			validateInput: text => {
+				return text === '' ? 'Do not leave this blank' : null;
+			}
+		});
+		mvPluginGenerator(`${titleOutput}`, `${authorOutput}`, `${infoOutput}`);
 }));
 
 	//this basically creates a blank RMMV Plugin file
